f1eea8fa2ebd1c215c9513830c3f34a3ae7165f1
Eetu Oinonen <eetuoi@metropolia.fi>
2024-12-04 15:10:34 +0200

    login page added

--- no-auth/frontend/src/pages/LoginPage.jsx
+++ no-auth/frontend/src/pages/LoginPage.jsx
@@ -1,12 +1,46 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import "../styles/LoginPage.css";
 
-const NotFound = () => {
-  return (
-    <div className="not-found">
-      <h2>Login</h2>
+const LoginPage = () => {
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        username: "",
+        password: "",
+    });
+    
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
 
-    </div>
-  );
-}
- 
-export default NotFound; 
+    const submitForm = async (e) => {
+        e.preventDefault();
+        navigate("/");
+    };
+    return (
+
+        <div className="login-page">
+            <h2>Login</h2>
+            <form onSubmit={submitForm}>
+
+                <label htmlFor="username">Username</label>
+                <input type="text" name="username" value={formData.username} onChange={handleChange} required />
+
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" value={formData.password} onChange={handleChange} required />
+
+                <button type="submit">Login</button>
+            </form>
+            <div className="signup-redirect">
+                <p>New in here? Sign up to be a user</p>
+                <button onClick={() => navigate("/signup")}>Sign Up</button>
+            </div>
+        </div>
+    );
+};
+
+export default LoginPage;
